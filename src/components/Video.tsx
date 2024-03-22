e8a2cfce97e375e30f2a4d048f534b0cf51520ff
Dinesh Suthar <suthardinesh626@gmail.com>
2024-03-22 20:43:21 +0530

    youtube video frame error fixed

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,8 +3,9 @@
 const Video = () => {
     return (
         <>
-            <div  className="flex justify-center items-center" >
-                <iframe className="max-sm:h-auto max-md:h-full w-5/6" width="1236" height="695" src="https://www.youtube.com/embed/z1p9ypB58Zk" title="This is CRCE Formula Racing." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className="flex justify-center items-center" >
+                {/* <iframe className="max-sm:h-auto max-md:h-full w-5/6" width="1236" height="695" src="https://www.youtube.com/embed/z1p9ypB58Zk" title="This is CRCE Formula Racing." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+                <iframe className="max-sm:h-auto max-md:h-full w-5/6" width="1236" height="695" src="https://www.youtube.com/embed/z1p9ypB58Zk" title="This is CRCE Formula Racing." allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
             </div>
         </>
     )
