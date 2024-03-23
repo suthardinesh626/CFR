a32cc437cab276e6e7794575477ca5df6baa1d66
Dinesh Suthar <suthardinesh626@gmail.com>
2024-03-23 16:42:46 +0530

    removied unused code

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,7 +4,6 @@ const Video = () => {
     return (
         <>
             <div className="flex justify-center items-center" >
-                {/* <iframe className="max-sm:h-auto max-md:h-full w-5/6" width="1236" height="695" src="https://www.youtube.com/embed/z1p9ypB58Zk" title="This is CRCE Formula Racing." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
                 <iframe className="max-sm:h-auto max-md:h-full w-5/6" width="1236" height="695" src="https://www.youtube.com/embed/z1p9ypB58Zk" title="This is CRCE Formula Racing." allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
             </div>
         </>
